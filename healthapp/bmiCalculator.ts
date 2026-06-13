5a189a734ea531a25369215af4be52bc5ea9401c
40412 <jasmin.franti@gmail.com>
2026-06-13 22:03:00 +0300

    3

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,17 +1,34 @@
-export const calculateBmi = (heightCm: number, weightKg: number): string => {
-  const heightM = heightCm / 100;
-  const bmi = weightKg / (heightM * heightM);
+export const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
 
-  if (bmi < 18.5) {
-    return "Underweight";
-  }
-  if (bmi < 25) {
-    return "Normal range";
-  }
-  if (bmi < 30) {
-    return "Overweight";
-  }
+  if (bmi < 18.5) return "Underweight";
+  if (bmi < 25) return "Normal range";
+  if (bmi < 30) return "Overweight";
   return "Obese";
 };
 
-console.log(calculateBmi(180, 74));
+const parseBmiArguments = (args: string[]) => {
+  if (args.length !== 4) {
+    throw new Error("Expected height and weight as arguments");
+  }
+
+  const height = Number(args[2]);
+  const weight = Number(args[3]);
+
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error("Provided values were not numbers!");
+  }
+
+  return { height, weight };
+};
+
+if (process.argv[1] === import.meta.filename) {
+  try {
+    const { height, weight } = parseBmiArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (e: unknown) {
+    let msg = "Something went wrong.";
+    if (e instanceof Error) msg += " Error: " + e.message;
+    console.log(msg);
+  }
+}
--- healthapp/exerciseCalculator.ts
+++ healthapp/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-interface ExerciseResult {
+interface Result {
   periodLength: number;
   trainingDays: number;
   success: boolean;
@@ -11,7 +11,7 @@ interface ExerciseResult {
 export const calculateExercises = (
   dailyHours: number[],
   target: number,
-): ExerciseResult => {
+): Result => {
   const periodLength = dailyHours.length;
   const trainingDays = dailyHours.filter((h) => h > 0).length;
   const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength;
@@ -40,4 +40,26 @@ export const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+const parseExerciseArguments = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  const target = Number(args[2]);
+  const hours = args.slice(3).map((n) => Number(n));
+
+  if (isNaN(target) || hours.some((h) => isNaN(h))) {
+    throw new Error("Provided values were not numbers!");
+  }
+
+  return { target, hours };
+};
+
+if (process.argv[1] === import.meta.filename) {
+  try {
+    const { target, hours } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(hours, target));
+  } catch (e: unknown) {
+    let msg = "Something went wrong.";
+    if (e instanceof Error) msg += " Error: " + e.message;
+    console.log(msg);
+  }
+}
