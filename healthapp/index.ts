eb4b1bc10aa909e150134edc297017a006dda6af
40412 <jasmin.franti@gmail.com>
2026-06-14 10:36:56 +0300

    6-8

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator.ts";
+import { calculateExercises } from "./exerciseCalculator.ts";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -26,7 +28,31 @@ app.get("/bmi", (req, res) => {
   });
 });
 
-const PORT = 3003;
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const body: any = req.body;
+
+  if (!body.daily_exercises || !body.target) {
+    return res.status(400).send({ error: "parameters missing" });
+  }
+
+  if (
+    !Array.isArray(body.daily_exercises) ||
+    body.daily_exercises.some((d: unknown) => isNaN(Number(d))) ||
+    isNaN(Number(body.target))
+  ) {
+    return res.status(400).send({ error: "malformatted parameters" });
+  }
+
+  const daily = body.daily_exercises.map((d: unknown) => Number(d));
+  const target = Number(body.target);
+
+  const result = calculateExercises(daily, target);
+
+  return res.send(result);
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
